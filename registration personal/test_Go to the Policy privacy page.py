98bc6470ac4e3bd6aec2b19d6ed5ec3a4cc69d8f
ilyagubinqa <ilyagubin1234567@gmail.com>
2023-09-08 15:47:29 +0300

    first action

--- registration personal/test_Go to the Policy privacy page.py
+++ registration personal/test_Go to the Policy privacy page.py
@@ -8,7 +8,7 @@ import time
 import pytest
 
 # Открытие браузера и переход на страницу регистрации
-driver_service = Service(executable_path="C:\Program Files\Webdriver\chromedriver-win64\chromedriver.exe")
+driver_service = Service(executable_path="C:\\Program Files\\Webdriver\\chromedriver-win64\\chromedriver.exe")
 driver = webdriver.Chrome(service=driver_service)
 driver.maximize_window()
 driver.get('https://app.staging1.clickadilla.com/register')
