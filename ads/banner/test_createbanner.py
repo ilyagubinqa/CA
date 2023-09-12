cdd5d66786d83522790a3791b2959588bbd6348c
ilyagubinqa <ilyagubin1234567@gmail.com>
2023-09-12 15:01:36 +0300

    test

--- ads/banner/test_createbanner.py
+++ ads/banner/test_createbanner.py
@@ -14,7 +14,7 @@ driver.maximize_window()
 driver.get('https://app.staging1.clickadilla.com/login')
 
 # Ожидание появления полей и ввод данных для авторизации
-wait = WebDriverWait(driver, 55)
+wait = WebDriverWait(driver, 50)
 login_input = wait.until(EC.element_to_be_clickable((By.ID, "selenium-test-login-email-field")))
 login_input.send_keys('test_selenium04@gmail.com')
 password_input = wait.until(EC.element_to_be_clickable((By.ID, "selenium-test-login-password-field")))
