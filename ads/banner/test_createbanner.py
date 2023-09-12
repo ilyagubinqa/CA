3380aaf67c0b99cf7088b41bd637481a26050c15
ilyagubinqa <ilyagubin1234567@gmail.com>
2023-09-12 17:20:20 +0300

    test

--- ads/banner/test_createbanner.py
+++ ads/banner/test_createbanner.py
@@ -10,6 +10,7 @@ import time
 # Открытие браузера и переход на страницу регистрации
 driver_service = Service(executable_path="C:\\Program Files\\Webdriver\\chromedriver-win64\\chromedriver.exe")
 driver = webdriver.Chrome()
+driver.maximize_window()
 driver.get('https://app.staging1.clickadilla.com/login')
 
 # Ожидание появления полей и ввод данных для авторизации
