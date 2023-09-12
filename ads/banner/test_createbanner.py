bed5fa42c60d0efeb8040dd8e1603ad2f77f7520
ilyagubinqa <ilyagubin1234567@gmail.com>
2023-09-12 17:41:20 +0300

    test

--- ads/banner/test_createbanner.py
+++ ads/banner/test_createbanner.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 import time
 
+
 # Открытие браузера и переход на страницу регистрации
 driver_service = Service(executable_path="C:\\Program Files\\Webdriver\\chromedriver-win64\\chromedriver.exe")
 driver = webdriver.Chrome()
@@ -13,7 +14,7 @@ driver.maximize_window()
 driver.get('https://app.staging1.clickadilla.com/login')
 
 # Ожидание появления полей и ввод данных для авторизации
-wait = WebDriverWait(driver, 50)
+wait = WebDriverWait(driver, 55)
 login_input = wait.until(EC.element_to_be_clickable((By.ID, "selenium-test-login-email-field")))
 login_input.send_keys('test_selenium04@gmail.com')
 password_input = wait.until(EC.element_to_be_clickable((By.ID, "selenium-test-login-password-field")))
