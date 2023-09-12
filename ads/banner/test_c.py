141376ba125cc1c1676c3f61e721c36d1ca434ee
ilyagubinqa <ilyagubin1234567@gmail.com>
2023-09-12 11:57:18 +0300

    test

--- ads/banner/test_c.py
+++ ads/banner/test_c.py
@@ -12,23 +12,4 @@ driver = webdriver.Chrome(service=driver_service)
 driver.maximize_window()
 driver.get('https://app.staging1.clickadilla.com/login')
 
-# Ожидание появления полей и ввод данных для авторизации
-wait = WebDriverWait(driver, 50)
-login_input = wait.until(EC.element_to_be_clickable((By.ID, "selenium-test-login-email-field")))
-login_input.send_keys('test_selenium04@gmail.com')
-password_input = wait.until(EC.element_to_be_clickable((By.ID, "selenium-test-login-password-field")))
-password_input.send_keys('test_selenium04@gmail.com')
-send_button = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "v-btn__content")))
-send_button.click()
-
-# Вывод сообщения о создании креатива
-time.sleep(3)
-status_element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div[3]/div/div/div[3]/div/div/div[1]/button/span')))
-status = status_element.text
-
-if status == "Start Another Campaign":
-    print("Your Ad Campaign created successfully")
-else:
-    print("Failed to created Ad Campaign")
-
 sleep(50)
