783d5c026064c0587ec7de96e03c6df1ea6b3f7f
ilyagubinqa <ilyagubin1234567@gmail.com>
2023-09-12 14:49:39 +0300

    test

--- ads/banner/test_c.py
+++ ads/banner/test_c.py
@@ -1,15 +1,17 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from time import sleep
-import time
+from selenium.webdriver.chrome.options import Options
+import pytest
 
-# Открытие браузера и переход на страницу регистрации
-driver_service = Service(executable_path="C:\\Program Files\\Webdriver\\chromedriver-win64\\chromedriver.exe")
-driver = webdriver.Chrome(service=driver_service)
-driver.maximize_window()
-driver.get('https://app.staging1.clickadilla.com/login')
 
-sleep(50)
+@pytest.fixture()
+def browser():
+    options = Options()
+    options.add_argument('--headless')
+    chrome_browser = webdriver.Chrome(options=options)
+    return chrome_browser
+
+
+def test_button_exist(browser):
+    browser.get('https://www.qa-practice.com/elements/button/simple')
+    assert browser.find_element(By.ID, 'submit-id-submit').is_displayed()
