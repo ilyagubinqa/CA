c8a0e2bda54b95dcb1aad59fc36c561eb1c7fa8a
ilyagubinqa <ilyagubin1234567@gmail.com>
2023-09-12 18:30:18 +0300

    test

---
+++ simple tests/test_qa.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+import pytest
+
+
+@pytest.fixture()
+def browser():
+    driver_service = Service(executable_path="C:\Program Files\Webdriver\chromedriver-win64\chromedriver.exe")
+    chrome_browser = webdriver.Chrome(service=driver_service)
+    chrome_browser.implicitly_wait(10)
+    return chrome_browser
+
+
+def test_button1_exist(browser):
+    browser.get('https://www.qa-practice.com/elements/button/simple')
+    assert browser.find_element(By.ID, 'submit-id-submit').is_displayed()
+
+
+def test_button1_clicked(browser):
+    browser.get('https://www.qa-practice.com/elements/button/simple')
+    browser.find_element(By.ID, 'submit-id-submit').click()
+    assert 'Submitted' ==browser.find_element(By.ID, 'result-text').text
+
+def test_button2_exist(browser):
+    browser.get('https://www.qa-practice.com/elements/button/like_a_button')
+    assert browser.find_element(By.LINK_TEXT, 'Click').is_displayed()
+
