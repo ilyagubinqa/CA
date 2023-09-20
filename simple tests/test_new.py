8d0c787cadf3f995342765cb4902caf84f869de5
ilyagubinqa <ilyagubin1234567@gmail.com>
2023-09-20 17:50:59 +0300

    test new

--- simple tests/test_new.py
+++ simple tests/test_new.py
@@ -7,12 +7,10 @@ from selenium.webdriver.chrome.options import Options
 import time
 import pytest
 
-# Опции для запуска в режиме headless
-options = Options()
-options.add_argument('--headless')
-
 @pytest.fixture()
 def browser():
+    options = Options()
+    options.add_argument('--headless')
     driver_service = Service(executable_path="C:\\Program Files\\Webdriver\\chromedriver-win64\\chromedriver.exe")
     chrome_browser = webdriver.Chrome(service=driver_service, options=options)
     chrome_browser.implicitly_wait(12)
