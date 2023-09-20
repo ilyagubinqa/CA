b61cdec4e7131e26539b30541ffb6e84e1272ce5
ilyagubinqa <ilyagubin1234567@gmail.com>
2023-09-20 18:01:14 +0300

    test new

--- simple tests/test_new.py
+++ simple tests/test_new.py
@@ -1,18 +1,14 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
-import time
+
 import pytest
 
 @pytest.fixture()
 def browser():
     options = Options()
     options.add_argument('--headless')
-    driver_service = Service(executable_path="C:\\Program Files\\Webdriver\\chromedriver-win64\\chromedriver.exe")
-    chrome_browser = webdriver.Chrome(service=driver_service, options=options)
+    chrome_browser = webdriver.Chrome(options=options)
     chrome_browser.implicitly_wait(12)
     return chrome_browser
 
