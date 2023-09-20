30148cd8853eb5b20ba5632b1c0ad5d4191fdc2f
ilyagubinqa <ilyagubin1234567@gmail.com>
2023-09-20 13:57:05 +0300

    test new

--- simple tests/test_new.py
+++ simple tests/test_new.py
@@ -1,8 +1,12 @@
-import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
+from selenium.webdriver.chrome.options import Options
+import time
+import pytest
 
 # Опции для запуска в режиме headless
 options = Options()
