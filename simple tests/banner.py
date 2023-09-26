f620a7ba1b3703b9f7dfbe81c0df932560463b08
ilyagubinqa <ilyagubin1234567@gmail.com>
2023-09-26 14:49:30 +0300

    test element

--- simple tests/banner.py
+++ simple tests/banner.py
@@ -12,7 +12,7 @@ def browser():
     options = Options()
     options.add_argument('--headless')
     chrome_browser = webdriver.Chrome(options=options)
-    chrome_browser.implicitly_wait(13)
+    chrome_browser.implicitly_wait(12)
     return chrome_browser
 
 
