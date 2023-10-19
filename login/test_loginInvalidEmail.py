dca4660e1c4236407a957607f5fb5e451bc59444
ilyagubinqa <ilyagubin1234567@gmail.com>
2023-10-19 13:47:05 +0300

    test element

--- login/test_loginInvalidEmail.py
+++ login/test_loginInvalidEmail.py
@@ -33,11 +33,5 @@ def test_invalidemail(browser):
     time.sleep(15)
     error_element = browser.find_element(By.CLASS_NAME, "v-messages__message")
     error_message = error_element.text
+    print(error_message)
 
-    expected_error_message = "These credentials do not match our records."
-
-    if error_message.strip() == expected_error_message.strip():
-        print("Test passed successfully")
-    else:
-        print("Test failed")
-    time.sleep(35)
