49d9f6a7e9a2b847a98cb0f9ed2f734aca688711
ilyagubinqa <ilyagubin1234567@gmail.com>
2023-10-19 16:41:05 +0300

    test element

--- login/logininvalidemaildone.py
+++ login/logininvalidemaildone.py
@@ -9,6 +9,7 @@ import pytest
 @pytest.fixture()
 def browser():
     options = Options()
+    options.add_argument('--headless')
     chrome_browser = webdriver.Chrome(options=options)
     chrome_browser.implicitly_wait(35)
     return chrome_browser
