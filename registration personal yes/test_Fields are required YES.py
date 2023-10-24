2d2ff52e91fd79a18a0ee38091e3251f495ff3b8
ilyagubinqa <ilyagubin1234567@gmail.com>
2023-10-24 17:30:55 +0300

    test 1

--- registration personal yes/test_Fields are required YES.py
+++ registration personal yes/test_Fields are required YES.py
@@ -11,7 +11,7 @@ def browser():
     options = Options()
     options.add_argument('--headless')
     chrome_browser = webdriver.Chrome(options=options)
-    chrome_browser.implicitly_wait(25)
+    chrome_browser.implicitly_wait(26)
     return chrome_browser
 
 def test_registration(browser):
