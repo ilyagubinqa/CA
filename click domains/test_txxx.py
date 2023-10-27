a649c61d4ee62baa843211e2380e020b7d5f483f
ilyagubinqa <ilyagubin1234567@gmail.com>
2023-10-27 15:51:29 +0300

    test 1

--- click domains/test_txxx.py
+++ click domains/test_txxx.py
@@ -23,8 +23,8 @@ def test_domain(browser):
     time.sleep(5)
     status_element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="__nuxt"]/div/div/main/section[2]/div/div[1]/div[1]/div[1]/p')))
     status = status_element.text
-
-    if status == "test":
+    print(status)
+    if status == "Total visit":
         print("Test passed successfully")
     else:
         print("Test failed")
