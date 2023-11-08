a4b38152b1437e4059163c9a21fe23ea274b2674
ilyagubinqa <ilyagubin1234567@gmail.com>
2023-11-08 14:02:57 +0300

    test click txxx

--- click domains/test_txxx.py
+++ click domains/test_txxx.py
@@ -25,8 +25,6 @@ def test_domain(browser):
 
     # Проверка на отображение блока с текстом
     time.sleep(5)
-
-    # Your existing test code
     status_element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="__nuxt"]/div/div/main/section[2]/div/div[1]/div[1]/div[1]/p')))
     status = status_element.text
     print(status)
