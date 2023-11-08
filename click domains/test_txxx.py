8e037e103af61245f5d324d69d095455f56b8bfd
ilyagubinqa <ilyagubin1234567@gmail.com>
2023-11-08 16:14:34 +0300

    test click txxx

--- click domains/test_txxx.py
+++ click domains/test_txxx.py
@@ -25,7 +25,7 @@ def test_domain(browser):
 
     # Проверка на отображение блока с текстом
     time.sleep(6)
-    status_element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="__nuxt"]/div/div/main/section[2]/div/div[1]/div[1]/div[1]/p')))
+    status_element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="__nudxt"]/div/div/main/section[2]/div/div[1]/div[1]/div[1]/p')))
     status = status_element.text
     print(status)
     result = "Test passed successfully" if status == "Total visits" else "Test failed"
