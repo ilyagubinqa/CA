6b937a754c2530a9e199a347bda810ed1e0239b3
ilyagubinqa <ilyagubin1234567@gmail.com>
2023-11-09 12:03:53 +0300

    test click txxx

--- click domains/test_txxx.py
+++ click domains/test_txxx.py
@@ -21,7 +21,7 @@ def browser():
 def test_domain(browser):
     # Открытие браузера и переход на страницу премиум сайта
     browser.maximize_window()
-    browser.get('https://сlick.txxx.com')
+    browser.get('https://click.txxx.com')
 
     # Проверка на отображение блока с текстом
     time.sleep(5)
