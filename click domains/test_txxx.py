673f960ce40d9ffbb65f97a86215332669c3ec3c
ilyagubinqa <ilyagubin1234567@gmail.com>
2023-11-09 11:57:13 +0300

    test click txxx

--- click domains/test_txxx.py
+++ click domains/test_txxx.py
@@ -24,13 +24,13 @@ def test_domain(browser):
     browser.get('https://cсlick.txxx.com')
 
     # Проверка на отображение блока с текстом
-    time.sleep(6)
+    time.sleep(5)
     status_element = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="__nuxt"]/div/div/main/section[2]/div/div[1]/div[1]/div[1]/p')))
     status = status_element.text
     print(status)
     result = "Test passed successfully" if status == "Total visits" else "Test failed"
 
-    # Сохранение результата теста в файле
+    # Сохранение результата теста
     with open("test_results.txt", "w") as file:
         file.write(result)
     time.sleep(5)
