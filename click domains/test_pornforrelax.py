da13aa425cf3ad4f9d14c127e3020f4be3506c81
ilyagubinqa <ilyagubin1234567@gmail.com>
2023-11-17 17:15:00 +0300

    test click domains

--- click domains/test_pornforrelax.py
+++ click domains/test_pornforrelax.py
@@ -21,7 +21,7 @@ def test_domain(browser):
     try:
         # Открытие браузера и переход на страницу премиум сайта
         browser.maximize_window()
-        browser.get('https://click.pornforrelax.com')
+
 
         # Проверка на отображение блока с текстом
         time.sleep(5)
